bcbdb03a40bf6a8bc10fe548b448ed1e4f9f3ce0
Samgamingv2 <samuelimanuelpranata@gmail.com>
2026-06-05 18:43:36 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from pygame import *
+class gameSorite(sprite.Sprite):
+  def __init__(self, player_image, player_x, player_y, player_seed, width, height):
+    super().__init()
+    self.image = transform.scale(image.load(player_image). (width, height))
+    self.rect = self.image.get_rect()
+    self.rect.x = player_x
+    self.rect.y = player_y
+  def reset(self):
+    window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+  def update_r(self):
+    keys = key.get_pressed()
+    if key[K_UP] and self.rect.y > 5:
+      self.rect.y -= self.speed
+    if keys[K_DOWN] and self.rect.y < win_height - 80:
+      self.rect.y +=self.speed
+  def update_(self):
+    keys = key.get_pressed()
+    if key[K_w] and self.rect.y > 5:
+      self.rect.y -= self.speed
+    if keys[K_s] and self.rect.y < win_height - 80:
+      self.rect.y +=self.speed
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racketi = Player('racket.png', "30,200", 4, 50, 50, 150)
+racket2 = Player('racket.png', "30,200", 4, 50, 50, 150)
+ball = GameSprite("tenis_ball.png", 200, 200, 4, 50,50)
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('Player 1 Lose!' , True, (180, 0,0)
+lose2 = font.render('Player 1 Lose!' , True, (180, 0,0)
+speed_x = 3
+speed_y = 3
